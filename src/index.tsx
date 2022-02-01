3c73fbc04e79240c3ab2c9e44ebdef556a69c00a
Manuel Debić <manuel.debic@med.uni-heidelberg.de>
2022-02-01 21:29:44 +0100

    Remove unnecessary dependencies and only install production depenencies in docker container

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
 import ReactDOM from "react-dom";
 import App from "./App";
-import {Provider} from "react-redux";
-import store from "./store/store"
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
